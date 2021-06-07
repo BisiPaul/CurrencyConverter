d61ea06277e4f8cbdb120b2a69eeaf7e19f3ab66
Paul <paul.bisioc@tazz.ro>
2021-06-07 10:58:22 +0300

    - add unit tests for RatesRepository

--- app/src/main/java/com/bisipaul/currencyconverter/data/api/models/rates/Rates.kt
+++ app/src/main/java/com/bisipaul/currencyconverter/data/api/models/rates/Rates.kt
@@ -38,6 +38,12 @@ data class Rates(
     val USD: Double?,
     val ZAR: Double?
 ) {
+    constructor(list: List<Double>) : this (
+        list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7], null, list[8], list[9], list[10], list[11],
+        list[12], list[13], list[14], list[15], list[16], list[17], list[18], list[19], list[20], list[21],
+        list[22], list[23], list[24], list[25], list[26], list[27], list[28], list[29], list[30]
+    )
+
     fun toList(): MutableList<Rate> {
         val list = mutableListOf<Rate>()
         AUD?.let {
--- app/src/test/java/com/bisipaul/currencyconverter/data/api/repositories/rates/RatesRepositoryTest.kt
+++ app/src/test/java/com/bisipaul/currencyconverter/data/api/repositories/rates/RatesRepositoryTest.kt
@@ -1,8 +1,68 @@
 package com.bisipaul.currencyconverter.data.api.repositories.rates
 
-import junit.framework.TestCase
+import com.bisipaul.currencyconverter.core.ErrorResponse
+import com.bisipaul.currencyconverter.core.SuccessResponse
+import com.bisipaul.currencyconverter.data.api.models.apiResponse.GetRatesResponse
+import com.bisipaul.currencyconverter.data.api.models.rates.Rates
+import io.mockk.*
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.random.Random
+
 
 /**
  * Created by paulbisioc on 07.06.2021
  */
-class RatesRepositoryTest : TestCase()
+class MainViewModelTest {
+
+    private var repository: RatesRepository = mockk(relaxed = true)
+
+    @Before
+    fun setup() {
+    }
+
+    private fun setupSuccessRepository() = runBlocking {
+        val randomDoubles = List(31) { Random.nextDouble(0.0, 10.0)}
+        coEvery { repository.getRates("EUR") } returns SuccessResponse(GetRatesResponse("EUR", Rates(randomDoubles)))
+    }
+
+    private fun setupErrorRepository() = runBlocking {
+        coEvery { repository.getRates("err") } returns ErrorResponse()
+    }
+
+    @After
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `test list of rates has always size of 31`() {
+        setupSuccessRepository()
+
+        runBlocking {
+            when (val response = repository.getRates("EUR")) {
+                is SuccessResponse -> {
+                    assertEquals(31, response.data?.rates?.toList()?.size)
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `test list of rates with wrong baseCurrency returns ErrorResponse`() {
+        setupErrorRepository()
+        var flag = false
+
+        runBlocking {
+            when (val response = repository.getRates("err")) {
+                is ErrorResponse -> {
+                    flag = true
+                }
+            }
+            assert(flag)
+        }
+    }
+}
